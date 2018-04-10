df6b2f90adbf252ee3b7e63000e357cd93c74045
xiaoburning <977690289@qq.com>
2018-04-10 15:12:15 +0800

    可滚动式图、可移动区域

---
+++ pages/movable/movable.js
@@ -0,0 +1,78 @@
+// pages/movable/movable.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    x: 0,
+    y: 0
+  },
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    });
+  },
+  onChange: function (e) {
+    console.log(e.detail)
+  },
+  onScale: function (e) {
+    console.log(e.detail)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
